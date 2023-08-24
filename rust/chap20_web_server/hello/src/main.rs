729b3de5f705347f3e55bd0f9172f9191139bbff
Thales Carl Lavoratti <thalescl@esss.co>
2023-08-23 22:22:33 -0300

    Handle connection and print http request

--- rust/chap20_web_server/hello/src/main.rs
+++ rust/chap20_web_server/hello/src/main.rs
@@ -1,10 +1,25 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // bind is anologous to new in other structs
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection established");
+
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader // Vec<_> is using a placeholder to what collect() returns
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty()) // Checks if two lines in a row came empty? Or just one line came empty?
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
