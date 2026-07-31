603a479bbd71e5ce2e7f1a91b24059ef2d170e67
ThalesCarl <tclavoratti@gmail.com>
2026-07-31 10:57:48 -0300

    Add pressure force calculation
    WIP: it`s serial and there`s an error

--- sph/main.py
+++ sph/main.py
@@ -17,14 +17,18 @@ from dataclasses import dataclass
 
 # Constants
 GRAVITY = 9.81 # [m/s2]
-BOX_HEIGHT = 10.0
-BOX_WIDTH = 10.0
-RADIUS = 0.3
-COLLISION_DAMPING = 1.0
-NUM_PARTICLES = 5
-BETWEEN_PARTICLE_SPACING = 0.0
-SMOOTHING_RADIUS = 1.0
-MASS = 1.0
+BOX_HEIGHT = 10.0 # [m]
+BOX_WIDTH = 10.0 # [m]
+RADIUS = 0.3 # [m]
+COLLISION_DAMPING = 1.0 # [-]
+NUM_PARTICLES = 5 # [-]
+BETWEEN_PARTICLE_SPACING = 0.0 # [m]
+SMOOTHING_RADIUS = 1.0 # [m]
+MASS = 1.0 # [kg]
+TARGET_DENSITY = 1.0 # [kg/m2]
+PRESSURE_MULTIPLIER = 1.0 # [m4/s2]
+
+DEBUG = False
 
 def main():
     pl = pv.Plotter()
@@ -34,6 +38,7 @@ def main():
     # velocities: list[np.ndarray] = []
     positions: np.ndarray = np.zeros(shape=(NUM_PARTICLES, 3), dtype=float)
     velocities: np.ndarray = np.zeros(shape=(NUM_PARTICLES, 3), dtype=float)
+    densities: np.ndarray = np.zeros(NUM_PARTICLES, dtype=float)
     circles = start_particles_random(pl, positions)
     # draw_particles(pl, positions)
 
@@ -54,12 +59,10 @@ def main():
             if pl.render_window is None:
                 break
             
-            update(positions, velocities, delta_t, circles)
-            # draw(original_points, circles, positions)
-            # print(circles)
-            # if frame == 1:
-            #     pl.show()
-            #     sys.exit()
+            if DEBUG and frame == 0:
+                pl.show()
+                sys.exit()
+            update(positions, velocities, densities, delta_t, circles)
 
             # Crucial: Tell PyVista to redraw the scene
             pl.update()
@@ -69,6 +72,7 @@ def main():
 
     except Exception as e:
         print(f"Loop interrupted: {e}")
+        raise
 
     finally:
         # Ensure resources are closed out properly if stopped
@@ -91,6 +95,7 @@ def configure_plotter(pl: pv.Plotter) -> None:
     pl.view_xy()
     pl.disable_shadows()
     pl.disable_anti_aliasing()
+    pl.render_window.SetPosition(1920, 0)
     # pl.show_axes()
     # pl.show_bounds()
     pl.show(interactive_update=True)
@@ -139,13 +144,25 @@ def start_particles_random(pl: pv.Plotter, positions: np.ndarray) -> list[pv.Pol
         circles.append(circle)
     return circles
 
-def update(positions: np.ndarray, velocities: np.ndarray, delta_t: float, circles: list[pv.PolyData]) -> None:
-    for (position, velocity, circle) in zip(positions, velocities, circles):
-        velocity[1] += -1.0 * GRAVITY * delta_t
-        position += velocity * delta_t
-        resolve_collisions(position, velocity)
+def update(positions: np.ndarray, velocities: np.ndarray, densities: np.ndarray, delta_t: float, circles: list[pv.PolyData]) -> None:
+    # Serial
+    for idx in range(NUM_PARTICLES):
+    # for (position, velocity, density, circle) in zip(positions, velocities, densities, circles):
+        velocities[idx][1] += -1.0 * GRAVITY * delta_t # [m/s]
+        densities[idx] = calculate_density(positions[idx], positions) # [kg/m2]
+
+        # pressure_force = np.array([0.0, 0.0, 0.0])
+        pressure_force = calculate_pressure_force(idx, positions, densities) # [kg.m/s2]
+
+        # Why divide by density instead of mass?
+        pressure_acceleration = pressure_force / density # [kg.m/s2] * [m2/kg] = [m3/s2]
+        velocities[idx] += pressure_acceleration * delta_t # [m3/s] - got wrong units
+
+        positions[idx] += velocities[idx] * delta_t # [m/s] * [s] = [m]
+        resolve_collisions(positions[idx], velocities[idx])
 
         circle.points += velocity * delta_t
+    # Parallel
     
 
 def draw(original_points: list[pv.PolyData], circles: list[pv.PolyData], positions: np.ndarray) -> None:
@@ -171,41 +188,69 @@ def resolve_collisions(position: np.ndarray, velocity: np.ndarray):
         velocity[1] *= -1.0 * COLLISION_DAMPING
         
 def smoothing_kernel(dst: float) -> float:
-    value = max(0.0, SMOOTHING_RADIUS * SMOOTHING_RADIUS - dst * dst)
+    value = max(0.0, SMOOTHING_RADIUS * SMOOTHING_RADIUS - dst * dst) # [m2]
 
-    # Used to normalize the value. It's called volume but in 2D it has a area unit
-    volume = math.pi * math.pow(SMOOTHING_RADIUS, 8) / 4.0
-    return value * value * value / volume
+    # Used to normalize the value.
+    volume_factor = math.pi * math.pow(SMOOTHING_RADIUS, 8) / 4.0 # [m8]
+
+    # It's called volume but in 2D it has a area unit
+    return value * value * value / volume_factor  # [1/m2]
 
 def smoothing_kernel_derivative(dst: float) -> float:
     if dst >= SMOOTHING_RADIUS:
         return 0.0
     
-    value = max(0.0, SMOOTHING_RADIUS * SMOOTHING_RADIUS - dst * dst)
-    scale = - 24.0 / (math.pi * math.pow(SMOOTHING_RADIUS, 8))
-    return scale * dst * value * value
+    value = max(0.0, SMOOTHING_RADIUS * SMOOTHING_RADIUS - dst * dst) # [m2]
+    scale = - 24.0 / (math.pi * math.pow(SMOOTHING_RADIUS, 8)) # [1/m8]
+    return scale * dst * value * value # [1/m3]
 
 
 
 def calculate_density(sample_point: np.ndarray, positions: np.ndarray):
     density = 0.0
     for position in positions:
-        dst = np.norm(sample_point - position)
-        influence = smoothing_kernel(dst)
-        density += MASS * influence
+        dst = np.linalg.norm(sample_point - position) # [m]
+        influence = smoothing_kernel(dst) # [1/m2]
+        density += MASS * influence # [kg/m2]
     
-    return density
+    return density # [kg/m2]
 
 def calculate_property(sample_point: np.ndarray, positions: np.ndarray, particle_properties: np.ndarray) -> float:
+    """General function with SPH method to compute any property"""
     particle_property = 0.0
 
     for i, position in enumerate(positions):
-        dst = np.norm(sample_point - position)
+        dst = np.linalg.norm(sample_point - position)
         influence = smoothing_kernel(dst)
         density = calculate_density(sample_point, positions)
         particle_property += particle_properties[i] * influence * MASS / density
 
     return particle_property
+
+def calculate_pressure_force(particle_idx: int, positions: np.ndarray, densities: np.ndarray) -> np.ndarray:
+    pressure_force = np.array([0.0, 0.0, 0.0])
+    sample_point = positions[particle_idx]
+
+    for i, position in enumerate(positions):
+        if i == particle_idx:
+            continue
+        dst = np.linalg.norm(sample_point - position) # [m]
+        direction = np.array([1.0, 0.0, 0.0]) # FIXME change to random direction
+        if dst > 1.0e-6:
+            direction = (sample_point - position) / dst
+        density = densities[i] # [kg/m2]
+        slope = smoothing_kernel_derivative(dst) # [1/m3]
+        pressure_factor = calculate_pressure_factor(density) # [kg/m2.s2]
+        pressure_force +=  direction * pressure_factor * slope *  MASS / density # [kg/m2.s2] * [1/m3] * [kg] * [m2/kg]
+
+    return pressure_force # [kg.m/s2]
+
+def calculate_pressure_factor(density: float) -> float:
+    density_error = density - TARGET_DENSITY # [kg/m2]
+    pressure_factor = PRESSURE_MULTIPLIER * density_error # [m4/s2] * [kg/m2]
+    return pressure_factor # [kg/m2.s2]
+
+    
 
 if __name__ == "__main__":
     main()
